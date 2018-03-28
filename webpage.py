ba993f9149ca59834c086624f3c2255a75587e00
spencrr <23708360+spencrr@users.noreply.github.com>
2018-03-28 15:02:09 -0500

    new webpage

--- webpage.py
+++ webpage.py
@@ -1,23 +1,16 @@
-from flask import Flask
-from flask import request
-from flask import render_template
-import stringComparison
+from flask import Flask, render_template
+import average
 
 app = Flask(__name__)
 
 @app.route('/')
-def my_form():
-    return render_template("index.html")
+def index():
+  return render_template('index.html')
 
-@app.route('/', methods=['POST'])
-def my_form_post():
+@app.route('/my-link/')
+def my_link():
+    average.main()
+    return 'Running...'
 
-text1 = request.form['text1']
-text2 = request.form['text2']
-plagiarismPercent = stringComparison.extremelySimplePlagiarismChecker(text1,text2)
-if plagiarismPercent > 50 :
-    return "<h1>Plagiarism Detected !</h1>"
-else :
-    return "<h1>No Plagiarism Detected !</h1>"
 if __name__ == '__main__':
-    app.run()
+  app.run()
