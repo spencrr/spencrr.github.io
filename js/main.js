01716133024de731dfd630c76d9449554fe8e4c3
spencrr <23708360+spencrr@users.noreply.github.com>
2018-11-17 18:13:05 -0600

    added typing and more styling

---
+++ js/main.js
@@ -0,0 +1,11 @@
+let typed = new Typed('.interests', {
+    strings: ['SpaceX Enthusiast', 'Maker', 'Developer',
+        'Tech Visionary', 'Computer Scientist', 'Photographer', 'Beta Tester'],
+    typeSpeed: 30,
+    backSpeed: 30,
+    startDelay: 500,
+    backDelay: 1500,
+    loop: true,
+    shuffle: true,
+    smartBackspace: true,
+})
