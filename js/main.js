6589411651e65ecb0089535cbe75578080955eab
Spencer <spencers0730@gmail.com>
2018-11-25 17:30:35 -0600

    added functionality to interact with IPLogger

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 let typed = new Typed('.interests', {
     strings: ['SpaceX Enthusiast', 'Maker', 'Developer',
-        'Tech Visionary', 'Computer Scientist', 'Photographer', 'Beta Tester'],
+        'Tech Visionary', 'Computer Scientist', 'Photographer', 'Beta Tester'
+    ],
     typeSpeed: 30,
     backSpeed: 30,
     startDelay: 500,
@@ -8,4 +9,17 @@ let typed = new Typed('.interests', {
     loop: true,
     shuffle: true,
     smartBackspace: true,
-})
+});
+
+let getIP = new XMLHttpRequest();
+getIP.open('GET', 'https://ipinfo.io/json', true);
+getIP.onload = () => {
+    let data = JSON.parse(getIP.responseText);
+    data['iat'] = new Date();
+    let send = new XMLHttpRequest();
+    send.open("POST", 'http://35.202.160.152:5000/api/log', true);
+    send.setRequestHeader('Content-Type', 'application/json');
+    send.send(JSON.stringify(data));
+
+};
+getIP.send();
