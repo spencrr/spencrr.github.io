abd059e2852fc0ad75386b6b8e7bb6e410bbc0e7
Spencer Schoenberg <spencers0730@gmail.com>
2020-01-08 15:45:07 -0600

    Add 'portfolio/' from commit 'afcdbff01af9094020dd3539f7d8eebe4aa0cbc9'
    git-subtree-dir: portfolio
    git-subtree-mainline: 0e0e11d18a6dd8e5fc1074ef1ca0759780fbc62f
    git-subtree-split: afcdbff01af9094020dd3539f7d8eebe4aa0cbc9

---
+++ portfolio/src/components/DarkModeButton.vue
@@ -0,0 +1,40 @@
+<template>
+  <v-tooltip top>
+    <template v-slot:activator="{ on }">
+      <v-icon
+        v-on="on"
+        @click="toggleDark"
+        class="glow mx-2"
+        color="secondary"
+        >{{ mdiDesktopMac }}</v-icon
+      >
+    </template>
+    <span>Super Secret Dark Mode 😉</span>
+  </v-tooltip>
+</template>
+
+<script>
+import { mdiDesktopMac } from "@mdi/js";
+
+export default {
+  name: "DarkModeButton",
+  data: () => ({ mdiDesktopMac }),
+  methods: {
+    toggleDark() {
+      this.$vuetify.theme.dark = !this.$vuetify.theme.dark;
+    },
+  },
+};
+</script>
+
+<style>
+.glow {
+  animation: beat 1s ease-in-out infinite alternate;
+}
+
+@keyframes beat {
+  to {
+    transform: scale(1.25);
+  }
+}
+</style>
---
+++ portfolio/src/views/Home.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div v-for="(section, index) in sections" :key="index">
+      <v-divider v-if="index != 0" />
+      <component :is="section" class="py-5" :id="section" />
+    </div>
+  </div>
+</template>
+
+<script>
+import About from "@/views/About";
+import Projects from "@/views/Projects";
+import Coursework from "@/views/Coursework";
+
+const components = { About, Projects, Coursework };
+const sections = Object.keys(components);
+
+export default {
+  name: "Home",
+  components,
+  data: () => ({ sections }),
+};
+export { sections };
+</script>
