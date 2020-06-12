89f3799e0dcd494eaf2b058f88d8787f7db5bee3
Spencer Schoenberg <spencers0730@gmail.com>
2020-06-12 11:39:59 -0500

    add date relevancy and move ts types

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -1,12 +1,4 @@
-export interface Course {
-  title: string;
-  descriptiveTitle: string;
-  institute: string;
-  display?: string;
-  subject: string;
-  number: string;
-  description: string;
-}
+import { Course, CourseSort } from "./types";
 
 const courses: Course[] = [
   {
@@ -18,6 +10,10 @@ const courses: Course[] = [
     number: "106B",
     description:
       "Abstraction and its relation to programming. Software engineering principles of data abstraction and modularity. Object-oriented programming, fundamental data structures (such as stacks, queues, sets) and data-directed design. Recursion and recursive data structures (linked lists, trees, graphs). Introduction to time and space complexity analysis. Uses the programming language C++ covering its basic facilities",
+    relevancy: {
+      date: new Date("2018-08"),
+      display: "Summer 2018",
+    },
   },
   {
     title: "Introduction to Scientific Computing",
@@ -28,6 +24,10 @@ const courses: Course[] = [
     number: "108",
     description:
       "Introduction to Scientific Computing Numerical computation for mathematical, computational, physical sciences and engineering: error analysis, floating-point arithmetic, nonlinear equations, numerical solution of systems of algebraic equations, banded matrices, least squares, unconstrained optimization, polynomial interpolation, numerical differentiation and integration, numerical solution of ordinary differential equations, truncation error, numerical stability for time dependent problems and stiffness. Implementation of numerical methods in MATLAB programming assignments.",
+    relevancy: {
+      date: new Date("2018-08"),
+      display: "Summer 2018",
+    },
   },
   {
     title: "Linear Algebra",
@@ -38,6 +38,10 @@ const courses: Course[] = [
     number: "383",
     description:
       "Topics include the use of elementary row operations to solve systems of linear equations, linear dependence, linear transformations, matrix operations, inverse of a matrix, determinants, subspaces, spaces, column spaces, dimension and rank, eigenvalues and eigenvectors, diagonalization of matrices, similarity, inner product and orthogonality, orthogonal projections and Gram-Schmidt process.",
+    relevancy: {
+      date: new Date("2019-01"),
+      display: "Winter 2019",
+    },
   },
   {
     title: "Calculus IV",
@@ -48,6 +52,10 @@ const courses: Course[] = [
     number: "232",
     description:
       "This course is a continuation of MA 231 and an introduction to multiple integration and infinite series. Topics include double and triple integrals with applications to areas, volumes and moments, infinite series with tests for convergence, power series, Taylor and Maclaurin series, and operations with series.",
+    relevancy: {
+      date: new Date("2018-11"),
+      display: "Fall 2018",
+    },
   },
   {
     title: "Programming II",
@@ -59,6 +67,10 @@ const courses: Course[] = [
     number: "300",
     description:
       "Introduces students to Object-Oriented Programming using classes and objects to solve more complex problems. Introduces array-based and linked data structures: including lists, stacks, and queues. Programming assignments require writing and developing multi-class (file) programs using interfaces, generics, and exception handling to solve challenging real world problems. Topics reviewed include reading/writing data and objects from/to files and exception handling, and command line arguments. Topics introduced: object-oriented design; class vs. object; create and define interfaces and iterators; searching and sorting; abstract data types (List,Stack,Queue,PriorityQueue(Heap),Binary Search Tree); generic interfaces (parametric polymorphism); how to design and write test methods and classes; array based vs. linked node implementations; introduction to complexity analysis; recursion.",
+    relevancy: {
+      date: new Date("2019-09"),
+      display: "Fall 2019",
+    },
   },
   {
     title: "Introduction to Cryptography",
@@ -69,6 +81,10 @@ const courses: Course[] = [
     number: "435",
     description:
       "Cryptography is the art and science of transmitting digital information in a secure manner. Provides an introduction to its technical aspects.",
+    relevancy: {
+      date: new Date("2019-09"),
+      display: "Fall 2019",
+    },
   },
   {
     title: "Programming III",
@@ -79,6 +95,10 @@ const courses: Course[] = [
     number: "400",
     description:
       "The third course in our programming fundamentals sequence. It presumes that students understand and use functional and object-oriented design and abstract data types as needed. This course introduces balanced search trees, graphs, graph traversal algorithms, hash tables and sets, and complexity analysis and about classes of problems that require each data type. Students are required to design and implement using high quality professional code, a medium sized program, that demonstrates knowledge and use of latest language features, tools, and conventions. Additional topics introduced will include as needed for projects: inheritance and polymorphism; anonymous inner classes, lambda functions, performance analysis to discover and optimize critical code blocks. Students learn about industry standards for code development. Students will design and implement a medium size project with a more advanced user-interface design, such as a web or mobile application with a GUI and event- driven implementation; use of version-control software.",
+    relevancy: {
+      date: new Date("2020-01"),
+      display: "Spring 2020",
+    },
   },
   {
     title: "Introduction to Computer Engineering",
@@ -89,6 +109,10 @@ const courses: Course[] = [
     number: "252",
     description:
       "Logic components built with transistors, rudimentary Boolean algebra, basic combinational logic design, basic synchronous sequential logic design, basic computer organization and design, introductory machine- and assembly-language programming.",
+    relevancy: {
+      date: new Date("2020-01"),
+      display: "Spring 2020",
+    },
   },
   {
     title: "Introduction to Discrete Mathematics",
@@ -99,6 +123,10 @@ const courses: Course[] = [
     number: "240",
     description:
       "Basic concepts of logic, sets, partial order and other relations, and functions. Basic concepts of mathematics (definitions, proofs, sets, functions, and relations) with a focus on discrete structures: integers, bits, strings, trees, and graphs.  Propositional logic, Boolean algebra, and predicate logic. Mathematical induction and recursion. Invariants and algorithmic correctness. Recurrences and asymptotic growth analysis. Fundamentals of counting.",
+    relevancy: {
+      date: new Date("2020-01"),
+      display: "Spring 2020",
+    },
   },
   {
     title: "AP Computer Science A",
@@ -108,6 +136,10 @@ const courses: Course[] = [
     number: "100",
     description:
       "AP Computer Science A is an introductory college-level computer science course. Students cultivate their understanding of coding through analyzing, writing, and testing code as they explore concepts like modularity, variables, and control structures.",
+    relevancy: {
+      date: new Date("2017-09"),
+      display: "Spring 2017",
+    },
   },
   {
     title: "Client-Side Internet Technologies",
@@ -118,32 +150,10 @@ const courses: Course[] = [
     number: "193C",
     description:
       "Client-side technologies used to create web sites such as Google maps or Gmail. Includes HTML5, CSS, JavaScript, the Document Object Model (DOM), and Ajax.",
-  },
-];
-
-export interface CourseSort {
-  compareTo(a: Course, b: Course): number;
-  display: string;
-}
-
-const sorts: CourseSort[] = [
-  {
-    compareTo: (a, b) => {
-      return a.number.localeCompare(b.number);
+    relevancy: {
+      date: new Date("2018-07"),
+      display: "Summer 2018",
     },
-    display: "Course Number",
-  },
-  {
-    compareTo: (a, b) => {
-      return a.title.localeCompare(b.title);
-    },
-    display: "Course Title",
-  },
-  {
-    compareTo: (a, b) => {
-      return a.institute.localeCompare(b.institute);
-    },
-    display: "Institution",
   },
 ];
 
@@ -153,13 +163,32 @@ const alphabeticalSort: CourseSort = {
   },
   display: "Course Title",
 };
-const complexSorts: CourseSort[] = sorts.map((sort) => {
-  const compareTo = (a: Course, b: Course) => {
-    const compare = sort.compareTo(a, b);
-    if (compare == 0) return alphabeticalSort.compareTo(a, b);
-    return compare;
-  };
 
+const sorts: CourseSort[] = [
+  {
+    compareTo: (a, b) => {
+      return b.relevancy.date.getTime() - a.relevancy.date.getTime();
+    },
+    display: "Relevancy",
+  },
+  {
+    compareTo: (a, b) => {
+      return a.number.localeCompare(b.number);
+    },
+    display: "Course Number",
+  },
+  alphabeticalSort,
+  {
+    compareTo: (a, b) => {
+      return a.institute.localeCompare(b.institute);
+    },
+    display: "Institution",
+  },
+];
+
+const complexSorts: CourseSort[] = sorts.map((sort) => {
+  const compareTo = (a: Course, b: Course) =>
+    sort.compareTo(a, b) || alphabeticalSort.compareTo(a, b);
   return { ...sort, compareTo };
 });
 
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,15 +1,4 @@
-interface Project {
-  title: string;
-  subtitle: string;
-  href: string;
-  media: "img" | "vid";
-  src(): string;
-  desc: string;
-}
-
-interface ProjectConfig {
-  getAnchorTag: (text: string, href: string) => string;
-}
+import { Project, ProjectConfig } from "./types";
 
 const projects = ({ getAnchorTag }: ProjectConfig): Project[] => [
   {
@@ -18,10 +7,12 @@ const projects = ({ getAnchorTag }: ProjectConfig): Project[] => [
     href: "https://spencrr.github.io/schelling-model/",
     media: "vid",
     src: () => require("@/assets/projects/schellings-model.mp4"),
-    desc: `The Visualization of ${getAnchorTag(
-      "Schelling's Model",
-      "https://wikipedia.org/wiki/Agent-based_model#1970s_and_1980s:_the_first_models"
-    )} was inspired by courses in Sociology and English. The main principle of the model is to show how residential segregation can arise from a small preference to be around members of the same group. In this visualization, each color represents a group. The preference is calculated as a frequency of surrounding pixels for each pixel.`,
+    desc: `The Visualization of ${
+      getAnchorTag(
+        "Schelling's Model",
+        "https://wikipedia.org/wiki/Agent-based_model#1970s_and_1980s:_the_first_models",
+      )
+    } was inspired by courses in Sociology and English. The main principle of the model is to show how residential segregation can arise from a small preference to be around members of the same group. In this visualization, each color represents a group. The preference is calculated as a frequency of surrounding pixels for each pixel.`,
   },
   {
     title: "Conway's Game of Life",
@@ -30,13 +21,17 @@ const projects = ({ getAnchorTag }: ProjectConfig): Project[] => [
       "https://github.com/spencrr/processing-projects/tree/master/Cellular%20Automata/GameOfLife",
     media: "vid",
     src: () => require("@/assets/projects/game-of-life.mp4"),
-    desc: `As a project for AP Computer Science, ${getAnchorTag(
-      "Conway's Game of Life",
-      "https://wikipedia.org/wiki/Conway%27s_Game_of_Life"
-    )} was written using ${getAnchorTag(
-      "Processing",
-      "https://processing.org/"
-    )}, a sketchbook for creating computational visual arts. Game of Life is a cellular automaton that uses basic rules to determine the state of each pixel iteratively.`,
+    desc: `As a project for AP Computer Science, ${
+      getAnchorTag(
+        "Conway's Game of Life",
+        "https://wikipedia.org/wiki/Conway%27s_Game_of_Life",
+      )
+    } was written using ${
+      getAnchorTag(
+        "Processing",
+        "https://processing.org/",
+      )
+    }, a sketchbook for creating computational visual arts. Game of Life is a cellular automaton that uses basic rules to determine the state of each pixel iteratively.`,
   },
   {
     title: "Julia Sets",
@@ -46,14 +41,18 @@ const projects = ({ getAnchorTag }: ProjectConfig): Project[] => [
     media: "vid",
     src: () => require("@/assets/projects/julia-sets.mp4"),
     desc: `This project helps create a visualization for
-            ${getAnchorTag(
-              "Julia Sets",
-              "https://wikipedia.org/wiki/Julia_set#Quadratic_polynomials"
-            )} of quadratic polynomials, like the famous Mandelbrot Set fractal. The
-            ${getAnchorTag(
-              "Mandelbrot Set",
-              "https://wikipedia.org/wiki/Mandelbrot_set"
-            )} is related to the Julia Sets by way of an iterative function that successively squares an input. Julia Sets use a specific complex constant for each point, whereas the Mandlebrot Set uses the point on the complex plane itself. This project is able to visualize both types of fractals!`,
+            ${
+      getAnchorTag(
+        "Julia Sets",
+        "https://wikipedia.org/wiki/Julia_set#Quadratic_polynomials",
+      )
+    } of quadratic polynomials, like the famous Mandelbrot Set fractal. The
+            ${
+      getAnchorTag(
+        "Mandelbrot Set",
+        "https://wikipedia.org/wiki/Mandelbrot_set",
+      )
+    } is related to the Julia Sets by way of an iterative function that successively squares an input. Julia Sets use a specific complex constant for each point, whereas the Mandlebrot Set uses the point on the complex plane itself. This project is able to visualize both types of fractals!`,
   },
   {
     title: "Fibonacci Spiral",
@@ -62,10 +61,12 @@ const projects = ({ getAnchorTag }: ProjectConfig): Project[] => [
       "https://github.com/spencrr/processing-projects/tree/master/Math%20Visualizations/FibonacciSpiral",
     media: "vid",
     src: () => require("@/assets/projects/fibonacci-spiral.mp4"),
-    desc: `The Fibonacci Spiral project is a direct implementation of the ${getAnchorTag(
-      "Golden Spiral",
-      "https://wikipedia.org/wiki/Golden_spiral"
-    )}, where an infinite sequence of squares create a spiral by drawing arcs through each square. The squares are wrapped around themselves with lengths corresponding to the elements in the Fibonacci Sequence.`,
+    desc: `The Fibonacci Spiral project is a direct implementation of the ${
+      getAnchorTag(
+        "Golden Spiral",
+        "https://wikipedia.org/wiki/Golden_spiral",
+      )
+    }, where an infinite sequence of squares create a spiral by drawing arcs through each square. The squares are wrapped around themselves with lengths corresponding to the elements in the Fibonacci Sequence.`,
   },
   {
     title: "This Portfolio",
