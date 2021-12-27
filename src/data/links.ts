51915a23b29f661d1d8a781f7f249b08c9a49cde
spencrr <23708360+spencrr@users.noreply.github.com>
2021-12-27 14:34:52 -0600

    update links.ts

---
+++ src/data/links.ts
@@ -0,0 +1,26 @@
+import { Link } from "./types";
+
+const links: Link[] = [
+    {
+        title: "Github",
+        src: "github.svg",
+        href: "https://github.com/spencrr",
+    },
+    {
+        title: "LinkedIn",
+        src: "links/linkedin.png",
+        href: "https://linkedin.com/in/spencer-schoenberg",
+    },
+    {
+        title: "Handshake",
+        src: "links/handshake.png",
+        href: "https://wisc.joinhandshake.com/users/18724712",
+    },
+    {
+        title: "Contact Me",
+        src: "links/mail.png",
+        href: "mailto:me@spencrr.dev",
+    },
+];
+
+export default links;
